efbef6f52b2d6d2b216898fccca24eee2bbdfd24
vasilytry <vsankov@gmail.com>
2024-10-09 18:32:17 +0700

    redistributed data_files

--- my/my.js
+++ my/my.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+//импортируем новую версию express-handlebars
 const { engine: expressHandlebars } = require('express-handlebars')
 
 const app = express()
@@ -7,38 +7,39 @@ const app = express()
 // configure Handlebars view engine
 app.engine('handlebars', expressHandlebars({
   defaultLayout: 'main',
+  //extname: '.hbs'
 }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'))
 
-
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.type('text/plain')
-  res.send('Meadowlark Travel');
-})
+app.get('/', (req, res) => res.render('home'))
+const fortunes = [
+  "Победи свои страхи, или они победят тебя.",
+  "Рекам нужны истоки.",
+  "Не бойся неведомого.",
+  "Тебя ждет приятный сюрприз.",
+  "Будь проще везде, где только можно.",
+]
 
+//app.get('/about', (req, res) => res.render('about'))
 app.get('/about', (req, res) => {
-  res.type('text/plain')
-  res.send('О Meadowlark Travel')
+  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+  res.render('about', { fortune: randomFortune })
 })
 
 // Пользовательская страница 404
 app.use((req, res) => {
-  res.type('text/plain')
   res.status(404)
-  res.send('404 — Не найдено')
+  res.render('404')
 })
-
-
-// Пользовательская страница 500 page
+// Пользовательская страница 500
 app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.type('text/plain')
+console.error(err.message)
   res.status(500)
-  res.send('500 - Server Error')
+  res.render('500')
 })
 
 app.listen(port, () => console.log(
---
+++ my/src/data_files/constants.ts
@@ -0,0 +1,40 @@
+//import ogImageSrc from "@./img/social.png";
+import ogImageSrc from "/img/social.png";
+
+export const SITE = {
+  title: "DokuHost - хостинг для Docker-контейнеров",
+  tagline: "Быстрый запуск и легкое масштабирование ваших Проектов",
+  description: "DokuHost.ru хостинг Docker-контейнеров, хостинг для разработчиков, быстрый деплой приложений и сервисов в docker-контейнерах",
+  description_short: "DokuHost.ru хостинг Docker-контейнеров для быстрого деплоя приложений и сервисов в docker-контейнерах",
+  url: "https://docuhost.ru",
+  author: "DokuHost.ru",
+};
+
+export const SEO = {
+  title: SITE.title,
+  description: SITE.description,
+  structuredData: {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    inLanguage: "ru-RU",
+    "@id": SITE.url,
+    url: SITE.url,
+    name: SITE.title,
+    description: SITE.description,
+    isPartOf: {
+      "@type": "WebSite",
+      url: SITE.url,
+      name: SITE.title,
+      description: SITE.description,
+    },
+  },
+};
+
+export const OG = {
+  locale: "ru_RU",
+  type: "website",
+  url: SITE.url,
+  title: `${SITE.title}: : Хостинг DokuHost для Docker-контейнеров`,
+  description: "Обеспечьте свои проекты быстрым и безопасным деплоем в контейнерах. Почувствуйте разницу благодаря ориентированному на пользователя дизайну и передовым инструментам. Начните исследовать уже сейчас!",
+  image: ogImageSrc,
+};
